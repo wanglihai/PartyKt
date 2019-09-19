9ac1bcd4c9a75fb2587607e07c7dd90dab54cc7a
wanglihai <androidwlh@163.com>
2019-09-19 16:57:14 +0800

    添加仿iOS弹窗

---
+++ lib_common/src/main/java/com/wlh/common/alertview/OnDismissListener.java
@@ -0,0 +1,9 @@
+package com.wlh.common.alertview;
+
+
+/**
+ * Created by Sai on 15/8/9.
+ */
+public interface OnDismissListener {
+     void onDismiss(Object o);
+}
---
+++ lib_common/src/main/java/com/wlh/common/alertview/OnItemClickListener.java
@@ -0,0 +1,9 @@
+package com.wlh.common.alertview;
+
+
+/**
+ * Created by Sai on 15/8/9.
+ */
+public interface OnItemClickListener {
+     void onItemClick(Object o, int position);
+}
